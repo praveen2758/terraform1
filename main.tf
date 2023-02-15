a4c912093f1b9f86611578e2644d12507b6bc9d3
praveen2758 <118465193+praveen2758@users.noreply.github.com>
2023-02-15 15:41:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-region= "us-east-1"
-access_key= "AKIARVBUNRK3CJ5VQLUP"
-secret_key= "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"
-}
 resource "aws_instance" "one" {
 ami= "ami-0b5eea76982371e91"
 instance_type= "t2.micro"
